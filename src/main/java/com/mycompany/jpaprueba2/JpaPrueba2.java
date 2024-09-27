5d129adb22240baeee52987a1b51569f48783a4c
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-26 20:46:17 -0500

    first commit

---
+++ src/main/java/com/mycompany/jpaprueba2/JpaPrueba2.java
@@ -0,0 +1,12 @@
+package com.mycompany.jpaprueba2;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class JpaPrueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
